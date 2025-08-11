d9b38f412d4aa2e96b50ec1b769c09c278501953
engmohesham <mohamedhesham695@gmail.com>
2025-08-12 00:09:08 +0300

    Update middleware configuration: Add StartSession middleware to the global middleware stack, remove VerifyCsrfToken from the middleware group, and adjust CSRF token exceptions to allow all routes. Additionally, remove 'api' middleware from RouteServiceProvider for improved API route handling.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Illuminate\Session\Middleware\StartSession::class,
     ];
 
     protected $middlewareGroups = [
@@ -21,7 +22,6 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
 
             Route::prefix('api')
-                ->middleware('api')
                 ->group(base_path('routes/api.php'));
         });
     }
