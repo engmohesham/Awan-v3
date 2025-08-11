d4a8aba2e91305f65399e55076c95c8d446c0253
engmohesham <mohamedhesham695@gmail.com>
2025-08-12 02:09:08 +0300

    Refactor User model for authentication: Remove JWT implementation and integrate Laravel Sanctum for API token management, enhancing security and simplifying user authentication.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
     use \Spatie\Permission\Traits\HasRoles;
 
     protected $fillable = [
@@ -29,15 +29,7 @@ class User extends Authenticatable implements JWTSubject
         'password' => 'hashed',
     ];
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
 
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 
     public function purchases()
     {
