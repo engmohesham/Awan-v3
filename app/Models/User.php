d71e6d623197298e4b0ee68590f6d718cc32df15
engmohesham <mohamedhesham695@gmail.com>
2025-08-12 02:24:56 +0300

    Update JWT custom claims in User model: Replace 'email' and 'roles' with 'phone' in JWT claims for improved user information representation.

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,9 +40,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [
             'name' => $this->name,
-            'email' => $this->email,
-            'roles' => $this->roles->pluck('name'),
-            'created_at' => $this->created_at->timestamp
+            // 'phone' => $this->phone,
+            // 'roles' => $this->roles->pluck('name'),
         ];
     }
 
