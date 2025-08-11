de397184a79125c21b8b367e3dc0396767751e7a
engmohesham <mohamedhesham695@gmail.com>
2025-08-11 21:45:20 +0300

    Update CORS configuration: Refined allowed paths to include 'admin/*', restricted allowed origins to 'https://awan.cowdly.sa', and enabled support for credentials.

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,12 @@
 <?php
 
 return [
-    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'admin/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['https://awan.cowdly.sa'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
