e914df957cc5a15181bea5081be4fb5b4c65ef50
engmohesham <mohamedhesham695@gmail.com>
2025-08-13 08:52:16 +0300

    Refactor CORS configuration and middleware integration: Remove CORS headers from nginx configuration, add CorsMiddleware to API kernel, and update CORS settings to disable support for credentials for improved security and compliance.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\CorsMiddleware::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
